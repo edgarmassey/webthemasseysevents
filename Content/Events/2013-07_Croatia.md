b1a3b066ff54a69ecc0c62c4a8bbe515b0f61a3b
EdgarMassey <58510315+EdgarMassey@users.noreply.github.com>
2026-01-27 11:46:49 +0100

    20260127
    Signed-off-by: EdgarMassey <58510315+EdgarMassey@users.noreply.github.com>

---
+++ Content/Events/2013-07_Croatia.md
@@ -0,0 +1,10 @@
+---
+title:2013-07 Croatia
+date: 2013-07
+location: Sweden 
+cover: 
+number: 32
+---
+
+
+Pictures from 2013-07
--- obj/Debug/net8.0/WebTheMasseysEvents.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebTheMasseysEvents.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTheMasseysEvents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b422717129571f4ec665dd438b4d25175a1c80f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93d87eacfb272d3b21548fcdab9f396ed9aacb2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTheMasseysEvents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTheMasseysEvents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
